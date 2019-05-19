4084d2c96ec97e07f14b5a0fee0533e5de64368d
kg4g18 <kg4g18@soton.ac.uk>
2019-05-19 15:25:19 +0100

    SQL Added EX12

---
+++ report/ex12.sql
@@ -0,0 +1,9 @@
+INSERT INTO Hotel(Hotel_ID,Overall_rating,Average_price,URL)
+	VALUES(11111,2.0,13,"test url1"),(11112,4,123,"test url2");
+
+INSERT INTO Author(User_ID,Author)
+        VALUES(1,"testAuthor1"),(2,"testAuthor2"),(3,"testAuthor3");
+
+INSERT INTO Review(Hotel_ID,User_ID,Content,Date,Number_of_readers,Number_of_helpful,Overall,Value,Rooms,Location,Cleanliness,Check_in_and_Front_desk,Service,Business_service)
+        VALUES(11111,1,"contentTest","dateTest",1,2,3,4,5,6,7,8,9,10),(11112,2,"contentTest","dateTest",1,2,3,4,5,6,7,8,9,10),(11111,3,"contentTest","dateTest",1,2,3,4,5,6,7,8,9,10);
+
